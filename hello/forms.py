e1b87be61eaf4fd30015947b522c599b437789fa
長友紀興 <kazuokinagatomo@kazuoki.local>
2023-09-02 04:15:42 +0900

    chapter2終了

--- hello/forms.py
+++ hello/forms.py
@@ -2,5 +2,27 @@ from django import forms
 
 class HelloForm(forms.Form):
     name = forms.CharField(label='name')
-    mail = forms.CharField(label='mail')
+    mail = forms.EmailField(label='mail')
     age = forms.IntegerField(label='age')
+
+    #チェックボックス(2択)
+    check = forms.BooleanField(label='Checkbox', required=False)
+
+    #３択
+    checks = forms.NullBooleanField(label='Checks')
+
+    data = [
+        ('one', 'item 1'),
+        ('two', 'item 2'),
+        ('three', 'item 3')
+    ]
+    #プルダウン
+    choice = forms.ChoiceField(label='choice', choices=data)
+
+    data_radio = [
+        ('one', 'radio 1'),
+        ('two', 'radio 2'),
+        ('three', 'radio 3')
+    ]
+    #ラジオボタン
+    choices = forms.ChoiceField(label='radio', choices=data_radio, widget=forms.RadioSelect())
--- hello/views.py
+++ hello/views.py
@@ -9,6 +9,10 @@ class HelloView(TemplateView):
             'title':'Hello',
             'message':'your data:',
             'form':HelloForm(),
+            'result':None,
+            'checks':None,
+            'choice':None,
+            'radio':None,
         }
     
     def get(self, request):
@@ -17,6 +21,16 @@ class HelloView(TemplateView):
     def post(self, request):
         msg = 'あなたは、<b>' + request.POST['name'] + '(' + request.POST['age'] + ')<b>さんです。<br>メールアドレスは<b>' + request.POST['mail'] + '<b>ですね。'
         self.params['message'] = msg
+        if 'check' in request.POST:
+            self.params['result'] = 'Checked!'
+        else:
+            self.params['result'] = 'not checked...'
+        chk = request.POST['checks']
+        self.params['checks'] = 'you selected: ' + chk
+        ch = request.POST['choice']
+        self.params['choice'] = 'selected: ' + ch
+        rad = request.POST['choices']
+        self.params['radio'] = 'selected: ' + rad
         self.params['form'] = HelloForm(request.POST)
         return render(request, 'hello/index.html', self.params)
 
