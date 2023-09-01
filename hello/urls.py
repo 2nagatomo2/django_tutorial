c061ce66d12a587b0f221cb9de3794f95afda355
長友紀興 <kazuokinagatomo@kazuoki.local>
2023-09-02 00:43:10 +0900

    2-2テンプレートの利用まで

--- hello/urls.py
+++ hello/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('next', views.next, name='next'),
+    path('<int:id>/<nickname>/', views.id_name),
+    path('my_name_is_<nickname>.I_am_<int:age>_years_old.', views.name_age)
 ]
--- hello/views.py
+++ hello/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    if 'msg' in request.GET:
-        msg = request.GET['msg']
-        result = 'you typed: "' + msg + '".'
-    else:
-        result = 'please send msg parameter!'
+    params = {
+        'title':'Hello/Index',
+        'msg':'これはサンプルで作ったページです。',
+        'goto':'next',
+    }
+    return render(request, 'hello/index.html', params)
+
+def next(request):
+    params = {
+        'title':'Hello/Next',
+        'msg':'これはもう一つのページです。',
+        'goto':'index',
+    }
+    return render(request, 'hello/index.html', params)
+
+def id_name(request, id, nickname):
+    result = 'your id: ' + str(id) + ', name: ' + nickname + '.'
+    return HttpResponse(result)
+
+def name_age(request, nickname, age):
+    result = 'your account: ' + nickname + '(' + str(age) + ').'
     return HttpResponse(result)
