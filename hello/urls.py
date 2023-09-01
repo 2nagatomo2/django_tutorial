f8ce77b0c6ef74fa5f9326e1b112cfb9ee0421bf
長友紀興 <kazuokinagatomo@kazuoki.local>
2023-09-02 03:19:42 +0900

    2-3 viewクラスの作成

--- hello/urls.py
+++ hello/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
+from .views import HelloView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('next', views.next, name='next'),
+    path(r'', HelloView.as_view(), name='index'),
     path('<int:id>/<nickname>/', views.id_name),
     path('my_name_is_<nickname>.I_am_<int:age>_years_old.', views.name_age)
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+from .forms import HelloForm
 
-def index(request):
-    params = {
-        'title':'Hello/Index',
-        'msg':'これはサンプルで作ったページです。',
-        'goto':'next',
-    }
-    return render(request, 'hello/index.html', params)
-
-def next(request):
-    params = {
-        'title':'Hello/Next',
-        'msg':'これはもう一つのページです。',
-        'goto':'index',
-    }
-    return render(request, 'hello/index.html', params)
+class HelloView(TemplateView):
+    def __init__(self) -> None:
+        self.params = {
+            'title':'Hello',
+            'message':'your data:',
+            'form':HelloForm(),
+        }
+    
+    def get(self, request):
+        return render(request, 'hello/index.html', self.params)
+    
+    def post(self, request):
+        msg = 'あなたは、<b>' + request.POST['name'] + '(' + request.POST['age'] + ')<b>さんです。<br>メールアドレスは<b>' + request.POST['mail'] + '<b>ですね。'
+        self.params['message'] = msg
+        self.params['form'] = HelloForm(request.POST)
+        return render(request, 'hello/index.html', self.params)
 
 def id_name(request, id, nickname):
     result = 'your id: ' + str(id) + ', name: ' + nickname + '.'
